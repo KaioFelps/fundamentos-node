78111ec62813d1ac43fef4e31dd8a22f3dcb62b2
Kaio Felipe <kaiofelipe2005@gmail.com>
2023-02-16 20:31:57 -0300

    :sparkles: create first server routes using native http module

--- src/server.js
+++ src/server.js
@@ -1,1 +1,34 @@
-console.log("Hello World")
+import http from "node:http"
+
+const server = http.createServer((req, res) => {
+    const { url, method } = req
+
+    if (url === "/users" && method === "GET") {
+        return res.end(JSON.stringify(
+            {
+                users: [
+                    {
+                        name: "Kaio Felipe",
+                        age: 17,
+                        phoneNumber: 44998379460,
+                    }
+                ]
+            }
+        ))
+    }
+
+    if (url === "/users" && method === "POST") {
+        return res.end(JSON.stringify({
+            success: true,
+            createdUser: {
+                name: "Teste",
+                age: 17,
+                phoneNumber: 44998379460,
+            }
+        }))
+    }
+
+    return res.end("Request sent")
+})
+
+server.listen(3333)
